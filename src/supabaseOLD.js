cf85fb2b7c1a79e5e219596efc67ae92a7b1d18c
Emily Plunkett <emilyjoyplunkett@gmail.com>
2025-09-04 13:38:50 -0400

    Remove duplicate supabaseOLD.js file causing multiple client instances

--- src/supabaseOLD.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://yrzugdmatddwypzdirhz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyenVnZG1hdGRkd3lwemRpcmh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2NDEwNzEsImV4cCI6MjA3MTIxNzA3MX0.2LUMRhFxfZW0jcr-DRajdPnajTQKwcQtAr_zraPZJd8'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
